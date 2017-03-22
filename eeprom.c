81aba134ba93115f2be6d8a8c86d93f7c6d55039
Thinh Le <timothyle85@gmail.com>
2017-03-21 18:35:50 -0700

    Update eeprom.c
    Added my helper functions for read and write
    Worked on mutators and accessors
    Static is being looked into
    Special function sync prototype in the comments

--- eeprom.c
+++ eeprom.c
@@ -1,5 +1,5 @@
 #include "IO_Driver.h"
-
+#include "IO_EEPROM.h"
 #include "eeprom.h"
 
 #define LOCAL static
@@ -41,31 +41,42 @@ EEPROMManager* EEPROMManager_new()
 // Mutators
 //---------------------------------------------------------------
 /**  @ingroup Mutators
-* @brief Specific description
+* @brief Sets the desired EEPROM values
 */
 bool EEPROMManager_set_ubyte1(EEPROMManager* me, eepromValue parameter, ubyte1* value)
 {
-
+    if(writeEP){
+        me -> value;
+    data_desired = *value;
+        return true;
+    }
+    return false;
 }
 
 //---------------------------------------------------------------
 // Accessors
 //---------------------------------------------------------------
 /**  @ingroup Accessors
-* @brief Specific description
+* @brief Gets the actual EEPROM values
 */
 bool EEPROMManager_get_ubyte1(EEPROMManager* me, eepromValue parameter, ubyte1* value)
 {
-
+    if(readEP){
+        me -> value;
+        *value = *data_actual;
+        return true;
+    }
+    return false;
 }
-
 
 //---------------------------------------------------------------
 // Special Functions
 //---------------------------------------------------------------
 IO_ErrorType EEPROMManager_sync(EEPROMManager* me)
 {
-    //
+    // if(data_actual!=data_desired){
+    //     data_actual = data_desired;
+    // }
 }
 
 
@@ -91,20 +102,50 @@ LOCAL bool getAddress(eepromValue value, ubyte2* address, ubyte1* bytes)
 }
 
 //Reads EEPROM and stores data in data_actual.  Waits until read is complete.
-LOCAL ? readInitialValues(???)
+LOCAL void readInitialValues(ubyte* data)  //might be void since we aren't modifying anything
 {
     //Read eeprom
     //Loop until status == ok
-    me->status = EEPROM_op_idle;
-
+    do{
+        me->status = EEPROM_op_idle;
+    } while(EEPROMManager_getStatus!=IO_E_OK);
+    *data_actual = *data;
 }
 
-LOCAL read(? ? ? )
-{
-    IO_EEPROM_Read()
+// Writes to EEPROM
+LOCAL void writeEP(ubyte2 offset, ubyte2 length, ubyte1 * data){
+    //checks if the EEPROM is busy
+
+    if(IO_EEPROM_GetStatus() == IO_E_OK){
+
+        //not busy starts reading
+
+        IO_EEPROM_Write(offset, length, data);
+
+    }
+
+    //data is busy not available
+
+    //needs IO_EEPROM_GetStatus 
+
+    //to return IO_E_OK 
 }
 
-LOCAL write(? ? ? )
-{
+// Reads to EEPROM
+LOCAL void readEP(ubyte2 offset, ubyte2 length, ubyte1 data){
+    //checks if the EEPROM is busy
 
+    if(IO_EEPROM_GetStatus() == IO_E_OK){
+
+        //not busy starts reading
+
+        IO_EEPROM_Read(offset, length, &data);
+
+    }
+
+    //data is busy not available
+
+    //needs IO_EEPROM_GetStatus 
+
+    //to return IO_E_OK 
 }
